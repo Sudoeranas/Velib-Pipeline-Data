4137df36abe4f2ea1c9f5f08c329442e59aa3689
Sudoeranas <anasmatata@gmail.com>
2026-05-24 21:03:06 +0200

    dbt pipeline complet

--- velib_dbt/models/staging/stg_availability.sql
+++ velib_dbt/models/staging/stg_availability.sql
@@ -2,27 +2,28 @@ with source as (
     select * from {{ source('raw', 'raw_availability') }}
 ),
 
-cleaned as (
+flattened as (
     select
-        stationcode::varchar                        as station_id,
-        name::varchar                               as station_name,
-        numdocksavailable::int                      as docks_available,
-        numbikesavailable::int                      as bikes_available,
-        mechanical::int                             as mechanical_bikes,
-        ebike::int                                  as electric_bikes,
-        is_installed::boolean                       as is_installed,
-        is_renting::boolean                         as is_renting,
-        is_returning::boolean                       as is_returning,
-        cast(duedate as timestamp_tz)               as updated_at,
-        cast(ingested_at as timestamp_tz)           as ingested_at,
-        coordonnees_geo:lat::float                  as latitude,
-        coordonnees_geo:lon::float                  as longitude
-    from source
-    where stationcode is not null
-      and numbikesavailable is not null
+        f.value:stationcode::varchar                as station_id,
+        f.value:name::varchar                       as station_name,
+        f.value:numdocksavailable::int              as docks_available,
+        f.value:numbikesavailable::int              as bikes_available,
+        f.value:mechanical::int                     as mechanical_bikes,
+        f.value:ebike::int                          as electric_bikes,
+        f.value:is_installed::boolean               as is_installed,
+        f.value:is_renting::boolean                 as is_renting,
+        f.value:is_returning::boolean               as is_returning,
+        f.value:duedate::timestamp_tz               as updated_at,
+        f.value:coordonnees_geo:lat::float          as latitude,
+        f.value:coordonnees_geo:lon::float          as longitude,
+        source.data:ingested_at::timestamp_tz       as ingested_at
+    from source,
+    lateral flatten(input => source.data:records) f
+    where f.value:stationcode is not null
+      and f.value:numbikesavailable is not null
 )
 
-select * from cleaned
+select * from flattened
 qualify row_number() over (
     partition by station_id, ingested_at
     order by updated_at desc
--- velib_dbt/models/staging/stg_stations.sql
+++ velib_dbt/models/staging/stg_stations.sql
@@ -2,21 +2,22 @@ with source as (
     select * from {{ source('raw', 'raw_stations') }}
 ),
 
-cleaned as (
+flattened as (
     select
-        stationcode::varchar                        as station_id,
-        name::varchar                               as station_name,
-        capacity::int                               as capacity,
-        coordonnees_geo:lat::float                  as latitude,
-        coordonnees_geo:lon::float                  as longitude,
-        nom_arrondissement_communes::varchar        as city_district,
-        cast(ingested_at as timestamp_tz)           as ingested_at
-    from source
-    where stationcode is not null
-      and capacity > 0
+        f.value:stationcode::varchar                    as station_id,
+        f.value:name::varchar                           as station_name,
+        f.value:capacity::int                           as capacity,
+        f.value:coordonnees_geo:lat::float              as latitude,
+        f.value:coordonnees_geo:lon::float              as longitude,
+        f.value:nom_arrondissement_communes::varchar    as city_district,
+        source.data:ingested_at::timestamp_tz           as ingested_at
+    from source,
+    lateral flatten(input => source.data:records) f
+    where f.value:stationcode is not null
+      and f.value:capacity::int > 0
 )
 
-select * from cleaned
+select * from flattened
 qualify row_number() over (
     partition by station_id
     order by ingested_at desc
